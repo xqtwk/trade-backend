76b8a9983dd56b0006c0a869b9e831039d25885e
xqtwk <xqtwkk@gmail.com>
2023-10-04 22:51:18 +0100

    just to test

---
+++ src/main/java/grade/tradeback/TestController.java
@@ -0,0 +1,15 @@
+package grade.tradeback;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Hi");
+    }
+}
