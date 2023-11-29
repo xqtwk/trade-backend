42b413bff92fa5b048cc8d19017d79bd32fd6c21
Richard <xqtwkk@gmail.com>
2023-11-29 19:58:05 +0300

    should be like that? secret can be null

--- src/main/java/grade/tradeback/user/User.java
+++ src/main/java/grade/tradeback/user/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,6 +29,7 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private boolean mfaEnabled;
+    @Nullable
     private String secret;
     
     @Enumerated(EnumType.STRING)
