cb6ce3bfd37a43e868600b61dc956ff076c69c85
Richard <xqtwkk@gmail.com>
2023-12-02 20:21:54 +0300

    password change implemented

---
+++ src/main/java/grade/tradeback/user/UserController.java
@@ -0,0 +1,25 @@
+package grade.tradeback.user;
+
+import grade.tradeback.user.dto.ChangePasswordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @PatchMapping
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordRequest request,
+            Principal connectedUser
+    ) {
+        userService.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
+    }
+}
